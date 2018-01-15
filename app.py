a0f6f39c250f0422de650c4ea56b836a1090e519
Andrew Miloslavsky <amiloslavsky@gmail.com>
2018-01-15 16:16:23 -0500

    Add portfolio

--- app.py
+++ app.py
@@ -3,41 +3,43 @@ import sys
 
 DB = 'coins.txt'
 
-start_flag = True
-
 try:
     while True:
         coinmarketcap = Market()
         
-        if start_flag:
-            with open(DB, 'r+') as f:
-                coins = [x.strip('\n') for x in f.readlines()]
-                
-                for coin in coins:
-                    print coin + ': $' + coinmarketcap.ticker(currency=coin)[0]['price_usd']
-            start_flag = False
-            print
+        with open(DB, 'r+') as f:
+            coins = [x.strip('\n') for x in f.readlines()]
+            
+            for coin in coins:
+                name = coin.split(' ')[0:-1][0]
+                quant = int(coin.split(' ')[-1])
+                price = float(coinmarketcap.ticker(currency=name)[0]['price_usd'])
+                print name + ': $' + str(price) +' * ' + str(quant) + ' = $' + str(quant * price)
+        print
 
-        cur = raw_input('\nEnter Name and Quantity: ')
-        # name = cur.split(' ')[0:-1][0]
-        # quant = cur.split(' ')[-1]
+        cur = raw_input()
         if len(cur):
             if cur[0] == '+':
                 with open(DB, 'a+') as f:
-                    f.write(cur[1:] + '\n')
-                    print cur[1:] + ' added succesfully.'
+                    f.write(cur.split(' ')[0:-1][0][1:] + ' ' + cur.split(' ')[-1] + '\n')
+                    print cur[1:] + ' added succesfully.\n'
             elif cur[0] == '-':
                 with open(DB, 'r+') as f:
                     coindb = f.readlines()
-                    if cur[1:]+'\n' in coindb:
-                        coindb.remove(cur[1:]+'\n')
+
+                    print coindb
+                    for idx, coin in enumerate(coindb):
+                        if cur[1:] in coindb[idx]:
+                            del coindb[idx]
+                            break
+
                     f.seek(0)
+                    
                     for entry in coindb:
                         f.write(entry)
+                 
                     f.truncate()
             else:
                 print "Value (USD): " + coinmarketcap.ticker(currency=cur)[0]['price_usd']
-        else:
-            continue
 except KeyboardInterrupt:
     pass
